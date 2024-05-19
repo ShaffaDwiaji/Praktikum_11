e7e78796a8166b20c5b1ebed6db401b1e4e1b44f
Shaffa Dwiaji <amagiri7141@gmail.com>
2024-05-19 22:43:38 +0700

    Praktikum 11

--- array.php
+++ array.php
@@ -1,23 +1,23 @@
 <!--Nomor 9-->
 <?php
 // Membuat array dengan nama dan umur
-$data = array(
-    array("nama" => "Shaffa", "umur" => 20),
-    array("nama" => "Ambatron", "umur" => 30),
-    array("nama" => "Rusdi", "umur" => 28),
-    array("nama" => "Bangkit", "umur" => 35),
-    array("nama" => "Devan", "umur" => 40),
-    array("nama" => "Eka", "umur" => 22),
-    array("nama" => "Fuad", "umur" => 29),
-    array("nama" => "Gilang", "umur" => 32),
-    array("nama" => "Hideung", "umur" => 27),
-    array("nama" => "Iwan", "umur" => 31),
-    array("nama" => "Jarwo", "umur" => 38),
-    array("nama" => "Kakap", "umur" => 26),
-    array("nama" => "Lesti", "umur" => 33),
-    array("nama" => "Mawar", "umur" => 34),
-    array("nama" => "Nami", "umur" => 36)
-);
+$data = [
+    ["nama" => "Shaffa", "umur" => 20],
+    ["nama" => "Ambatron", "umur" => 30],
+    ["nama" => "Rusdi", "umur" => 28],
+    ["nama" => "Bangkit", "umur" => 35],
+    ["nama" => "Devan", "umur" => 40],
+    ["nama" => "Eka", "umur" => 22],
+    ["nama" => "Fuad", "umur" => 29],
+    ["nama" => "Gilang", "umur" => 32],
+    ["nama" => "Hideung", "umur" => 27],
+    ["nama" => "Iwan", "umur" => 31],
+    ["nama" => "Jarwo", "umur" => 38],
+    ["nama" => "Kakap", "umur" => 26],
+    ["nama" => "Lesti", "umur" => 33],
+    ["nama" => "Mawar", "umur" => 34],
+    ["nama" => "Nami", "umur" => 36]
+];
 
 // Konversi array ke JSON
 $json_data = json_encode($data);
